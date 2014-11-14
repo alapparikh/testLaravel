b62dc79b8321ef34db3a5a1f6e25f78965620b95
alapparikh <alap18@gmail.com>
2014-11-13 21:37:31 -0500

    test

--- app/controllers/MobileController.php
+++ app/controllers/MobileController.php
@@ -50,7 +50,7 @@ class MobileController extends \BaseController {
 			$id = DB::table('mobiletokens')->select('user_id')->where('token','=',Input::get('token'))->get();
 			//$id = DB::select('select user_id from mobiletokens where token = ?', array(Input::get('token')));
 			//$links = DB::table('photos')->select('link'/*,'description','latitude','longitude','created_at'*/)->where('user_id','=',$id)->get();
-			$links = DB::table('photos')->select('link')->where('user_id','=',$id)->orderBy('created_at','desc')->get();
+			$links = DB::table('photos')->select('link')->where('user_id','=','7')->orderBy('created_at','desc')->get();
 		} catch (Exception $e){
 			return Response::json(['status' => 'failed']);
 		}
