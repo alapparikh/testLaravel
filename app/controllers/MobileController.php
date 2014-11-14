8e07aea0153dd59d13887453929a6a972d673ea6
alapparikh <alap18@gmail.com>
2014-11-13 21:58:58 -0500

    please work

--- app/controllers/MobileController.php
+++ app/controllers/MobileController.php
@@ -47,7 +47,7 @@ class MobileController extends \BaseController {
 
 	public function getphotos(){
 		try{
-			$id = DB::table('mobiletokens')->select('user_id')->where('token','=',Input::get('token'))->get();
+			$id = json_decode(DB::table('mobiletokens')->select('user_id')->where('token','=',Input::get('token'))->get());
 			//$id = DB::select('select user_id from mobiletokens where token = ?', array(Input::get('token')));
 			//$links = DB::table('photos')->select('link'/*,'description','latitude','longitude','created_at'*/)->where('user_id','=',$id)->get();
 			$links = DB::table('photos')->select('link')->where('user_id','=',$id->user_id)->orderBy('created_at','desc')->get();
