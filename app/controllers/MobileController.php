4a4736d562fd3f6f1483a395e400588c1ecc544c
alapparikh <alap18@gmail.com>
2014-11-13 20:46:35 -0500

    try get

--- app/controllers/MobileController.php
+++ app/controllers/MobileController.php
@@ -48,11 +48,11 @@ class MobileController extends \BaseController {
 	public function getphotos(){
 		try{
 			$id = DB::table('mobiletokens')->select('user_id')->where('token','=',Input::get('token'))->get();
-
-			$links = DB::table('photos')->select('link'/*,'description','latitude','longitude','created_at'*/)->where('user_id','=',$id)->orderBy('created_at','desc')->get();
+			//$id = DB::select('select user_id from mobiletokens where token = ?', array(Input::get('token')));
+			//$links = DB::table('photos')->select('link'/*,'description','latitude','longitude','created_at'*/)->where('user_id','=',$id)->get();
 		} catch (Exception $e){
 			return Response::json(['status' => 'failed']);
 		}
-		return $links;
+		return $id;
 	}
 }
