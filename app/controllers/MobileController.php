b8c22c4a9d9685264b91ceeb5c8ca07f80ae9f63
alapparikh <alap18@gmail.com>
2014-12-06 15:36:14 -0500

    testing api

--- app/controllers/MobileController.php
+++ app/controllers/MobileController.php
@@ -100,7 +100,7 @@ class MobileController extends \BaseController {
 		// get the result and parse to JSON
 		$result_arr = json_decode($result, true);
 
-		//$calories = $result_arr->hits[0]->fields->nf_calories;
+		$calories = $result_arr->result->hits[0]->fields->nf_calories;
 
 		return Response::json(['result' => $result_arr]);
 	}
